f7d1c5f104cfd3be59babbdf20fb0e1e864f75c3
Prerna-B <103260648+Prerna-B@users.noreply.github.com>
2024-04-26 18:55:16 +0530

    Update HelloController.java

--- src/main/java/com/example/HelloWorld/controller/HelloController.java
+++ src/main/java/com/example/HelloWorld/controller/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 	
 	@GetMapping("/hello")
 	public String hello() {
-		System.out.println("Testing");
+		System.out.println("Test");
        		return "Hello, World!";
     	}
 }
